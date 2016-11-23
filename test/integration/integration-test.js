0046101467f5c5a00ca7ae360b328b58ae12dfc1
Marc Bachmann <marc.brookman@gmail.com>
2016-11-23 02:08:32 +0100

    chore(package): update uuid to version 3.0.0

--- test/integration/integration-test.js
+++ test/integration/integration-test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 const async = require('async');
 const _ = require('lodash');
 const helper = require('../test-helper');
-const uuid = require('node-uuid');
+const uuid = require('uuid');
 const Joi = require('joi');
 
 chai.should();
